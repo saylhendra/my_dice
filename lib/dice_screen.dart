c7587e13d28bd7f2498b131086dcbf76131f19fd
Hendra Kurniawan <hendra.kurniawan88@japfa.com>
2024-02-16 05:10:37 +0700

    Adding fonts assets

--- lib/dice_screen.dart
+++ lib/dice_screen.dart
@@ -1,50 +1,33 @@
-import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class DiceScreen extends StatefulWidget {
+class DiceScreen extends StatelessWidget {
   const DiceScreen({super.key});
 
-  @override
-  State<DiceScreen> createState() => _DiceScreenState();
-}
-
-class _DiceScreenState extends State<DiceScreen> {
-  var _currentNumberDice = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Dice'),
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        title: const Text('My Dice'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: AnimatedCrossFade(
-              duration: const Duration(milliseconds: 500),
-              firstChild: Image.asset('assets/images/$_currentNumberDice.png', width: 150),
-              secondChild: Image.asset('assets/images/$_currentNumberDice.png', width: 150),
-              crossFadeState: _currentNumberDice == 1 ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-              // child: Center(
-              //   child: Image.asset('assets/images/$_currentNumberDice.png', width: 150),
-              // ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            ClipRRect(
+              child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5), child: Image.asset('assets/images/1.png', width: 200, height: 200)),
             ),
-          ),
-          Expanded(
-            flex: 0,
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _currentNumberDice = 1 + Random().nextInt(6);
-                });
-              },
-              child: const Text('Roll'),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                  '\"Roller dice\" could refer to a couple of things depending on the context, but it generally means dice that are rolled to generate random numbers, typically used in board games, role-playing games (RPGs), and educational activities. When you "roll dice" or use "roller dice," you\'re simply throwing one or more dice to see what numbers come up on the top-facing surfaces after they settle. This action is fundamental to many games and activities that require an element of chance or randomness.',
+                  style: TextStyle(fontSize: 22.0)),
             ),
-          ),
-          const Expanded(child: SizedBox(height: 20)),
-        ],
+          ],
+        ),
       ),
     );
   }
